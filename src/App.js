70eda6dd9a041c6c55a22d8fdd685e80c23c0a75
vLaauTy <96405301+vLaauTy@users.noreply.github.com>
2022-11-14 00:06:48 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/NavBar';
+import Home from './screens/Home';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ProductScreen from './screens/ProductScreen';
+import Badge from 'react-bootstrap/Badge';
+import { useContext } from 'react';
+import { Store } from './Store';
+import CartScreen from './screens/CartScreen';
+import './index.css'
 
 function App() {
+
+  const { state } = useContext(Store);
+  const { cart } = state
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='App'>
+      <NavBar />
+
+      <Routes>
+        <Route path='/cart' element={<CartScreen />} />
+        <Route path="/product/:_id" element={<ProductScreen />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
+
+
   );
 }
 
